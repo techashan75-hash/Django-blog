8acabeaa8162da34db3f4c0ef1d6995b081fac34
techashan75-hash <techashan75@gmail.com>
2026-02-11 01:21:54 +0300

    Update settings.py

--- Djangoproject/settings.py
+++ Djangoproject/settings.py
@@ -144,18 +144,16 @@ LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL ='login'
 
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-EMAIL_HOST =os.getenv('EMAIL_HOST')
-EMAIL_PORT =int(os.getenv('EMAIL_PORT'))
+EMAIL_HOST =os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT =int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS =os.getenv('EMAIL_USE_TLS') =='True'
 
-EMAIL_HOST_USER =os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER =os.getenv('EMAIL_HOST_USER','')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','')
 
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 EMAIL_TIMEOUT=10
-
-
 
 
 
