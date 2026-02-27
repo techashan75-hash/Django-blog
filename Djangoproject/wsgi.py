29a487fa6b5b871684dbe35fc9d6726966f60a35
techashan75-hash <techashan75@gmail.com>
2026-02-27 20:34:42 +0300

    Update wsgi.py

--- Djangoproject/wsgi.py
+++ Djangoproject/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Djangoproject.settings')
 
 application = get_wsgi_application()
+handler= application #vercel expects app/handler
